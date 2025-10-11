ca4506b5f0174aa4972746d3118199bc61a939b9
sho-Ada <popety.adachi@gmail.com>
2025-10-11 15:46:33 +0900

    8章２まで

--- src/main/java/com/example/controller/SignupController.java
+++ src/main/java/com/example/controller/SignupController.java
@@ -2,6 +2,7 @@ package com.example.controller;
 
 import java.util.Map;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -13,10 +14,14 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.example.application.service.UserApplicationService;
+import com.example.domain.user.model.MUser;
+import com.example.domain.user.service.UserService;
 import com.example.form.GroupOrder;
 import com.example.form.SignupForm;
 
 import lombok.extern.slf4j.Slf4j;
+
+
 
 @Controller
 @RequestMapping("/user")
@@ -26,6 +31,11 @@ public class SignupController {
 	@Autowired
 	private UserApplicationService userApplicationService;
 	
+	@Autowired
+	private UserService userService;
+	
+	@Autowired
+	private ModelMapper modelMapper;
 	/**ユーザー登録画面を表示*/
 	@GetMapping("/signup")
 	public String getSignup(Model model, @ModelAttribute SignupForm form) {
@@ -48,6 +58,13 @@ public class SignupController {
 		}
 		
 		log.info(form.toString());
+		
+		//formをMUserクラスに変換
+		MUser user = modelMapper.map(form, MUser.class);
+		
+		//ユーザー登録
+		userService.signup(user);
+		
 		//ログイン画面にリダイレクト
 		return "redirect:/login";
 	}
--- src/main/java/com/example/controller/UserListController.java
+++ src/main/java/com/example/controller/UserListController.java
@@ -1,17 +1,33 @@
 package com.example.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.example.domain.user.model.MUser;
+import com.example.domain.user.service.UserService;
 
 @Controller
 @RequestMapping("/user")
 public class UserListController {
 
+	@Autowired
+	private UserService userService;
+	
 	/**ユーザー画面を一覧表示*/
 	@GetMapping("/list")
-	public String getUserList() {
-
+	public String getUserList(Model model) {
+		
+		//ユーザー一覧を取得
+		List<MUser> userList = userService.getUsers();
+		
+		//modelに登録
+		model.addAttribute("userList", userList);
+		
 		//ユーザー一覧画面を表示
 		return "user/list";
 	}
